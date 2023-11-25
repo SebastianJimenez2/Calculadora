3b2bc896061cd67b9da0554bb14bfd8c2038259e
BryanLR <108194629+bryanLRx@users.noreply.github.com>
2023-11-25 11:12:05 -0500

    Agregar la clase Calculator y tests unitarios.

---
+++ src/main/java/org/example/git/calculator/Calculator.java
@@ -0,0 +1,37 @@
+package org.example.git.calculator;
+
+import javax.annotation.processing.SupportedAnnotationTypes;
+
+public class Calculator {
+    private int answer;
+    public int addition(int a, int b){
+        return a+b;
+    }
+    public int substraction(int a, int b){
+        return a-b;
+    }
+
+    @SuppressWarnings("IntegerDivisionInFloatingPointContext")
+    public int division(int a, int b){
+        return a/b;
+    }
+
+    public void timeOut(int time){
+        try{
+            Thread.sleep(time);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    public int getAnswer(){
+        return this.answer;
+    }
+    public void setAnswer(int answer){
+        this.answer=answer;
+    }
+
+
+
+
+}
